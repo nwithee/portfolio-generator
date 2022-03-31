4efd794bb3c09795f983439d514ca7bfc6a860b3
nzwithee@gmail.com <Cartoon#01>
2022-03-30 22:08:48 -0500

    updates

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
+//fs.writeFile('./index.html', pageHTML, err =>{
+    //if (err) throw err;
 
-
-fs.writeFile('index.html', generatePage(name, github), err =>{
-    if (err) throw err;
-
-    console.log('Porfolio complete!  Check out hte index.html to see the output!');
-});
+    //console.log('Porfolio complete!  Check out hte index.html to see the output!');
+//});
