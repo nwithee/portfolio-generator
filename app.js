74f0b94c54a15b4bc16fe735f985903d466c4075
nzwithee@gmail.com <Cartoon#01>
2022-03-30 21:26:53 -0500

    more updates

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-    
-    console.log('=============');
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err =>{
+    if (err) throw err;
+
+    console.log('Porfolio complete!  Check out hte index.html to see the output!');
+});
