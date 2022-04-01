b037b4dd7f8a5ccee3bb9e13055397b092a525b6
nzwithee@gmail.com <Cartoon#01>
2022-03-31 19:54:45 -0500

    Sure

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -139,10 +141,16 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
+        
     });
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
+
 
 //const pageHTML = generatePage(name, github);
 
